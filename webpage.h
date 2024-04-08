6ed757a1843695f1a0284650d623696c7f6bfe7b
Joel-Mendez0 <113056408+Joel-Mendez0@users.noreply.github.com>
2024-04-08 18:42:25 -0400

    Update webpage.h

--- webpage.h
+++ webpage.h
@@ -957,7 +957,7 @@ button { cursor: pointer; }
     </div>
     <!-- Video Stream Embedding -->
     <div class="video-stream-container" style="margin-top: 20px;">
-      <img src="http://192.168.202.124/stream" alt="Live Video Stream" style="width:100%; max-width:640px; border: 1px solid #ddd; border-radius: 4px; padding: 5px;">
+      <img src="http://172.20.10.8/stream" alt="Live Video Stream" style="width:100%; max-width:640px; border: 1px solid #ddd; border-radius: 4px; padding: 5px;">
     </div>
     <div class="chart-data-details">
       <div class="chart-details-header"></div>
